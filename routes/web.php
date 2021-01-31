c5d69f69b52368910c4204c0ebf0f7f362de2a2c
tech-o-zukky <tech.o.zukky@gmail.com>
2021-01-31 15:34:53 +0900

    ルーティングの基本

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,34 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('hello/{msg?}', function ($msg='no message.') {
+//    return '<html><body><h1>Hello</h1><p>This is sample page.</p></body></html>';
+//});
+
+$html = <<<EOF
+<html>
+<head>
+    <title>Hello</title>
+    <style>
+        body {font-size: 16pt; color: #999; }
+        h1 {font-size: 100pt; text-align: right; color: #eee; 
+            margin: -40px 0px -50px 0px; }
+    </style>
+</head>
+<body>
+    <h1>Hello</h1>
+    <!-- <p>This is sample page.</p> -->
+    <p>{$msg}</p>
+    <p>Laravel学習のためのサンプルページです.</p>
+</body>
+</html>
+EOF;
+
+//Route::get('hello', function () use ($html) {
+    return $html;
+});
+
+
+
+
