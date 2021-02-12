54a5b4b69466022938688bda42c793d426451456
tech-o-zukky <tech.o.zukky@gmail.com>
2021-02-12 18:38:21 +0900

    @forディレクティブの例

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -28,6 +28,19 @@
       <li>{{$item}}
         @endforeach
     </ol>
+
+    <p>&#064;forディレクティブの例</p>
+    <ol>
+      @for ($i = 1; $i < 100 ; $i++)
+        @if ($i % 2 == 1)
+          @continue
+        @elseif ($i <= 10)
+          <li>No, {{$i}}
+        @else
+          @break
+        @endif
+      @endfor
+    </ol>
 </body>
 
 </html>
