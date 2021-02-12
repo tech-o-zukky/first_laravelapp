4f6e4c814972e30365047e4d6648742d4622ce0d
tech-o-zukky <tech.o.zukky@gmail.com>
2021-02-12 18:45:17 +0900

    forディレクティブその2練習

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -41,6 +41,18 @@
         @endif
       @endfor
     </ol>
+
+    <p>&#064;forディレクティブの例 2</p>
+    @foreach($data as $item)
+    @if ($loop->first)
+      <p>※データ一覧</p><ul>
+    @endif
+    <li> No, {{$loop->iteration}}.{{$item}}</li>
+    @if ($loop->last)
+    </ul><p>-----ここまで</p>
+    @endif
+    @endforeach
+
 </body>
 
 </html>
