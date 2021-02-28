f05909a1c0f025b26828ee59b8b91658f1c2abde
tech-o-zukky <tech.o.zukky@gmail.com>
2021-02-28 15:20:02 +0900

    indexビュー(blade)修正

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -11,6 +11,17 @@
   <p>ここが本文のコンテンツです。</p>
   <p>必要なだけ記述できます。</p>
 
+<table>
+  <tr><th>Name</th><th>Mail</th><th>Age</th></tr>
+  @foreach ($items as $item)
+    <tr>
+      <td>{{$item->name}}</td>
+      <td>{{$item->mail}}</td>
+      <td>{{$item->age}}</td>
+    </tr>
+    @endforeach
+</table>
+
 @component('components.message')
   @slot('msg_title')
   CAUTION!
@@ -24,12 +35,14 @@
 
 @include('components.message', ['msg_title'=>'OK', 'msg_content'=>'サブビューです。'])
 
+{{-- //5-2
 <ul>
   @each('components.item', $data_eachtest, 'item')
 </ul>
 
 <p>Controller value<br>'message' = {{$message}}</p>
 <p>View value<br>'view_mesasge' = {{$view_message}}</p>
+--}}
 
 @endsection
 
