01bfb8d62133e47a89d3c94a9bf24ff54e3b571f
tech-o-zukky <tech.o.zukky@gmail.com>
2021-02-28 15:56:55 +0900

    DBupdate処理作成

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -84,4 +84,23 @@ class HelloController extends Controller
                     (:name, :mail, :age)', $param);
         return redirect('/hello');
     }
+
+    // 5-12 DBupdate
+    public function add(Request $request) {
+        $param = ['id' => $request->id];
+        $item = DB::select('select * from people where id = :id', $param);
+        return view('hello.edit', ['form' => $item[0]]);
+    }
+
+    // 5-12 DBupdate
+    public function update(Request $request) {
+        $param = [
+            'id' => $request->id,
+            'name' => $request->name,
+            'mail' => $request->mail,
+            'age' => $request->age,
+        ];
+        DB::update('update people set name =:name, mail =:mail, age =:age) where id =:id', $param);
+        return redirect('/hello');
+    }
 }
