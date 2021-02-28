ff9b6ad1717d1ded4eea1fbc3643a8350c2c4613
tech-o-zukky <tech.o.zukky@gmail.com>
2021-02-28 17:28:53 +0900

    seederファイル作成

--- database/seeders/PeopleTableSeeder.php
+++ database/seeders/PeopleTableSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class PeopleTableSeeder extends Seeder
 {
@@ -13,6 +14,47 @@ class PeopleTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        // 1
+        $param = [
+            'name' => 'taro',
+            'mail' => 'taro@yamada.jp',
+            'age' => 12,
+
+        ];
+        DB::table('people')->insert($param);
+        // 2
+        $param = [
+            'name' => 'hanako',
+            'mail' => 'hanako@shimada.com',
+            'age' => 35,
+
+        ];
+        DB::table('people')->insert($param);
+        // 3
+        $param = [
+            'name' => 'jiro',
+            'mail' => 'jiro@yokoyama.tv',
+            'age' => 48,
+
+        ];
+        DB::table('people')->insert($param);
+        
+        // 4
+        $param = [
+            'name' => 'kirishima',
+            'mail' => 'kirishima@kuro.net',
+            'age' => 53,
+
+        ];
+        DB::table('people')->insert($param);
+        
+        // 5
+        $param = [
+            'name' => 'goro',
+            'mail' => 'goro@lalala.co.jp',
+            'age' => 26,
+
+        ];
+        DB::table('people')->insert($param);
     }
 }
