6a3d395384faf95506de4f187856706ffc02c2ec
tech-o-zukky <tech.o.zukky@gmail.com>
2021-03-05 11:44:41 +0900

    PersonController修正
    記載漏れあったため追記
    ・indexアクションの記載
    ・Model\Personの読み込み

--- app/Http/Controllers/PersonController.php
+++ app/Http/Controllers/PersonController.php
@@ -2,10 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Person;
 use Illuminate\Http\Request;
 
 class PersonController extends Controller
 {
-    $items = Person::all();
-    return view('person.index', ['items' => $items]);
+    public function index(Request $request) {
+        $items = Person::all();
+        return view('person.index', ['items' => $items]);
+    }
 }
